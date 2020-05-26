72f5e9fecf8cacd285f19ca5ffff394a3d97b57c
Stepan Ermakov <stepanus97@yandex.ru>
2020-05-26 23:48:08 +0500

    temporary comment UseHttpsRedirection

--- MedicamentsAggregator.Service/Startup.cs
+++ MedicamentsAggregator.Service/Startup.cs
@@ -66,7 +66,7 @@ namespace MedicamentsAggregator.Service
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
